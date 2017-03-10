c46682781eed95bd6dba46976fafdbddd36f7378
wangqiliang <361526461@qq.com>
2017-03-10 16:23:09 +0800

    配置mybatis

--- src/main/java/com/wu/kislev/conf/MyBatisConfiguration.java
+++ src/main/java/com/wu/kislev/conf/MyBatisConfiguration.java
@@ -1,13 +1,9 @@
-package com.ws.kislev.conf;
+package com.wu.kislev.conf;
 
-import com.github.pagehelper.PageHelper;
-import org.apache.ibatis.plugin.Interceptor;
+
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
@@ -18,45 +14,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.transaction.annotation.TransactionManagementConfigurer;
 import org.springframework.web.context.request.RequestContextListener;
 
+import javax.annotation.Resource;
 import javax.sql.DataSource;
-import java.util.Properties;
 
 /**
  * MyBatis基础配置
  */
 @Configuration
-@EnableTransactionManagement
-public class MyBatisConfig implements TransactionManagementConfigurer {
-    public static final Logger LOGGER = LoggerFactory.getLogger(MyBatisConfig.class);
-    @Autowired
+public class MyBatisConfiguration {
+
+    @Resource(name="readDataSource1")
     DataSource dataSource;
 
-    @Bean(name = "sqlSessionFactory")
-    public SqlSessionFactory sqlSessionFactoryBean() {
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() {
         SqlSessionFactoryBean bean = new SqlSessionFactoryBean();
         bean.setDataSource(dataSource);
         bean.setTypeAliasesPackage("com.ws.kislev.model");
 
-        //分页插件
-        PageHelper pageHelper = new PageHelper();
-        Properties properties = new Properties();
-        properties.setProperty("reasonable", "true");
-        properties.setProperty("supportMethodsArguments", "true");
-        properties.setProperty("returnPageInfo", "check");
-        properties.setProperty("params", "count=countSql");
-        pageHelper.setProperties(properties);
-
-        //添加插件
-        bean.setPlugins(new Interceptor[]{pageHelper});
-
         //添加XML目录
         ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver();
-		bean.setConfigLocation(resolver.getResource("classpath:mybatis-config.xml"));
         try {
             bean.setMapperLocations(resolver.getResources("classpath:mapper/*.xml"));
             return bean.getObject();
         } catch (Exception e) {
-            LOGGER.error(e.getMessage(), e);
             throw new RuntimeException(e);
         }
     }
@@ -66,13 +47,5 @@ public class MyBatisConfig implements TransactionManagementConfigurer {
         return new SqlSessionTemplate(sqlSessionFactory);
     }
 
-    @Bean
-    @Override
-    public PlatformTransactionManager annotationDrivenTransactionManager() {
-        return new DataSourceTransactionManager(dataSource);
-    }
-    @Bean
-    public RequestContextListener requestContextListener(){
-        return new RequestContextListener();
-    }
+
 }
